f8b876625c4690ccde9a4ad3c38ca72883e60c64
Mohamed-Younes <mohamed.rouabehi.pro@gmail.com>
2019-07-14 22:10:42 +0200

    Rendu du module C05

---
+++ C05/ex02/ft_iterative_power.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/14 15:02:06 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/14 15:04:21 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	i;
+	int	res;
+
+	i = 1;
+	res = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (i++ <= power)
+	{
+		res = res * nb;
+	}
+	if (res > 2147483647)
+		return (0);
+	return (res);
+}
---
+++ C05/ex03/ft_recursive_power.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/14 15:05:50 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/14 15:07:46 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0 || nb > 2147483647)
+		return (0);
+	if (power == 0)
+		return (1);
+	if (power == 1)
+		return (nb);
+	return (nb * ft_recursive_power(nb, power - 1));
+}
