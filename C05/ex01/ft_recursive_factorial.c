80398f5f11c9aa909b9afeae85aeb16ce5fe9416
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-15 15:57:38 +0200

    Retrait de la gestion des int overflow

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -6,13 +6,13 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 14:59:42 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/14 15:01:09 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/15 15:57:15 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0 || nb > 12)
+	if (nb < 0)
 		return (0);
 	if (nb == 1 || nb == 0)
 		return (1);
