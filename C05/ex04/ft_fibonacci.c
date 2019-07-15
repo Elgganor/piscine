92d9d045b2457e06aa860a8cc028ecf5efeb8203
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-15 15:59:32 +0200

    Retrait de la gestion des int overflow

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 15:08:49 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/14 15:10:51 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/15 15:59:11 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@ int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	if (index >= 47)
-		return (0);
 	if (index < 2)
 		return ((index == 1) ? 1 : 0);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
