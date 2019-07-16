c1d9309226651876b21040cdbc840216fbd64f80
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-16 08:54:28 +0200

    Ne gere plus les int overflow

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/14 14:54:42 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/14 14:56:43 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/16 08:54:07 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	ft_iterative_factorial(int nb)
 	i = 0;
 	if (nb == 0 || nb == 1)
 		return (1);
-	if (nb < 0 || nb > 12)
+	if (nb < 0)
 		return (0);
 	while (i < nb)
 	{
