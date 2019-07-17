0eb3508c2b677b1e0ff0bb68bc14d4edeb5cd8ff
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-17 10:02:52 +0200

    Rendu du fichier ft.h

---
+++ C08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 08:58:37 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/17 10:02:32 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
