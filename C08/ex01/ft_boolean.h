9409daa0daa308a5be3f1cfb29bd290fcbb206d2
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-17 10:27:44 +0200

    Rendu de l'exercice ft_boolean.h

---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 10:07:40 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/17 10:26:57 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+enum	e_bool {FALSE, TRUE};
+typedef enum e_bool	t_bool;
+# define EVEN(nbr) (nbr % 2) == 0 ? 1 : 0
+# define EVEN_MSG  "I have an even number of arguents.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
+# define SUCCESS 0
+#endif
