307b77186d508b1f77d47d4b5d4c5f1b64ef257c
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-17 16:58:34 +0200

    Rendu de l'exercice ft_show_tab

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/17 16:24:01 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/17 16:56:08 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+void	ft_putnbr(int nb)
+{
+	long n;
+
+	n = nb;
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= (-1);
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + '0');
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	int	i;
+
+	i = 0;
+	while (par[i].size != 0)
+	{
+		ft_putnbr(par[i].size);
+		ft_putchar('\n');
+		ft_putstr(par[i].str);
+		ft_putchar('\n');
+		ft_putstr(par[i].copy);
+		ft_putchar('\n');
+		i++;
+	}
+}
