86a045dd97deaef1988c6f5884cc5a5da0004b0b
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-18 18:22:49 +0200

    Début de l'exercice ft_split

---
+++ C09/ex02/ft_split.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/18 17:24:37 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/18 17:55:55 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int		count_words(char *str, char *sep)
+{
+	int	cnt;
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	cnt = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (str[i + j] == sep[j])
+		{
+			if (sep[j + 1] == '\0')
+			{
+				i++;
+				j = -1;
+				cnt++;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (cnt + 1);
+}
+
+int	main(void)
+{
+	char *str = "Salutse";
+	char *sep = "se";
+
+	printf("%d\n", count_words(str, sep));
+	return (0);
+}
