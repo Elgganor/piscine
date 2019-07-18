53511e024ed95037b6a464969f71aa432ef80bd7
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-18 12:00:25 +0200

    Rendu d'un main pour le ft_strjoin

---
+++ C07/main/main3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str);
+char *ft_concat(char **strs, char *str, char *sep, int size);
+char	*ft_strjoin(int size, char **strs, char *sep);
+
+int	main(int ac, char **av)
+{
+	char *sep;
+	char *dest;
+
+	sep = ", ";
+	dest = ft_strjoin(ac, av, sep);
+
+	printf("%s\n", dest);
+	return (0);
+}
