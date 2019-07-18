19a298a10cb18e20604e8fe00b4501cc2b9c5e04
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-18 13:27:56 +0200

    Secure du malloc dans ft_strs_to_tab

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 13:52:24 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/17 17:01:14 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/18 13:27:32 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,7 +47,8 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	int			i;
 
 	i = 0;
-	tab_struct = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1));
+	if (!(tab_struct = (t_stock_str *)malloc(sizeof(t_stock_str) * (ac + 1))))
+		return (0);
 	while (i < ac)
 	{
 		tab_struct[i].size = ft_strlen(av[i]);
