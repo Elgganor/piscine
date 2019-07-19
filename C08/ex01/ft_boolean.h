bcde2c5cd5a20fc8993b9ed54ee11be9447d8596
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-19 13:41:56 +0200

    Correction du message de ft_boolean

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 10:07:40 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/19 10:45:26 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/19 13:41:13 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 enum	e_bool {FALSE, TRUE};
 typedef enum e_bool	t_bool;
 # define EVEN(nbr) (nbr % 2) == 0
-# define EVEN_MSG  "I have an even number of arguents.\n"
+# define EVEN_MSG  "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 #endif
