5345b695097db0f8c189339c5a807061790fb166
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-19 10:56:01 +0200

    Rendu de l'exercice ft_strs_to_tab

--- C08/ex04/ft_strs_to_tab.c
+++ C08/ex04/ft_strs_to_tab.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/17 13:52:24 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/19 10:48:20 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/19 10:55:35 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ struct s_stock_str	*ft_strs_to_tab(int ac, char **av)
 		return (0);
 	while (i < ac)
 	{
-		tab_struct[i].size = ft_strlen(av[i] - 1);
+		tab_struct[i].size = ft_strlen(av[i]) - 1;
 		tab_struct[i].str = av[i];
 		tab_struct[i].copy = ft_strdup(av[i]);
 		i++;
