709bd9b36a467b8e6eed5330292d5804b1ed0a66
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-21 14:58:47 +0200

    Rendu de la fonction ft_split

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -6,45 +6,116 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/18 17:24:37 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/20 19:09:14 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/21 14:58:13 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-int		count_words(char *str, char *sep)
+int		is_charset(char c, char *charset)
+{
+	int	i;
+
+	i = 0;
+	while (charset[i] != '\0')
+	{
+		if (c == charset[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int		count_word(char *str, char *charset)
 {
 	int	cnt;
 	int	i;
-	int	j;
 
-	j = 0;
-	i = 0;
 	cnt = 0;
-	while (str[i] != '\0')
+	i = 0;
+	while (*str)
 	{
-		j = 0;
-		while (str[i + j] == sep[j])
+		while (is_charset(*str, charset) == 1)
+			str++;
+		if (*str && is_charset(*str, charset) == 0)
 		{
-			if (sep[j + 1] == '\0')
-			{
-				i++;
-				j = -1;
-				cnt++;
-			}
-			j++;
+			cnt++;
+			while (*str && is_charset(*str, charset) == 0)
+				str++;
 		}
-		i++;
 	}
-	return (cnt + 1);
+	return (cnt);
 }
 
-int	main(void)
+char	*malloc_word(char *str, char *charset)
 {
-	char *str = "Foo  Bar Baz";
-	char *sep = " ";
+	int		len;
+	char	*tab_char;
+	int		i;
 
-	printf("%d\n", count_words(str, sep));
-	return (0);
+	len = 0;
+	i = 0;
+	while (str[i] != '\0' && is_charset(str[i], charset) == 0)
+	{
+		len++;
+		i++;
+	}
+	i = 0;
+	if (!(tab_char = (char *)malloc(sizeof(char) * (len + 1))))
+	{
+		return (0);
+	}
+	while(str[i] != '\0' && is_charset(str[i], charset) == 0)
+	{
+		tab_char[i] = str[i];
+		i++;
+	}
+	tab_char[i] = '\0';
+	return(tab_char);
+}
+
+char	**ft_split(char *str, char *charset)
+{
+	char	**tab;
+	int		i;
+	int		j;
+	int		wrds;
+
+	i = 0;
+	j = 0;
+	wrds = count_word(str, charset);
+	if (!(tab = (char **)malloc(sizeof(char *) * (wrds + 1))))
+		return (0);
+	while (str[i] != '\0')
+	{
+		while (str[i] && is_charset(str[i], charset) == 1)
+			i++;
+		if (str[i] && is_charset(str[i], charset) == 0)
+		{
+			tab[j] = malloc_word(&str[i], charset);
+			i++;
+			j++;
+		}
+	}
+	tab[j] = 0;
+	return (tab);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+	char **tab;
+
+	i = 0;
+	if (ac == 3)
+	{
+		tab = ft_split(av[1], av[2]);
+		while (i < count_word(av[1], av[2]))
+		{
+			printf("%s\n", tab[i]);
+			i++;
+		}
+	}
+	return  (0);
 }
