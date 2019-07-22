9218c2e0d6ac75b0e3fb4454cc56cb0ae2bb07f1
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-22 12:08:36 +0200

    Rendu de l'exercice do-op

---
+++ C11/ex05/srcs/do_op.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_op.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/22 10:44:10 by mrouabeh          #+#    #+#             */
+/*   Updated: 2019/07/22 11:39:00 by mrouabeh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "do-op.h"
+
+void	do_op(int a, char *c, int b, int (*f[])(int,int))
+{
+	if (c[0] == '+')
+		ft_putnbr((*f[0])(a,b));
+	else if (c[0] == '-')
+		ft_putnbr((*f[1])(a,b));
+	else if (c[0] == '*')
+		ft_putnbr((*f[2])(a,b));
+	else if (c[0] == '/')
+	{	
+		if (b == 0)
+			ft_putstr("Stop : division by zero");
+		else
+			ft_putnbr((*f[3])(a,b));
+	}
+	else if (c[0] == '%')
+	{
+		if (b == 0)
+			ft_putstr("Stop : modulo by zero");
+		else
+			ft_putnbr((*f[4])(a,b));
+	}
+	else
+		ft_putnbr(0);
+}
