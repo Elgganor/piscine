92994bf6863d6b05fac0ffde02ee720f3bca0301
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-23 10:37:08 +0200

    Rendu des main

---
+++ C11/main/main7.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void	ft_advanced_sort_string_tab(char **tab, int (*cmp)(char *, char *));
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	main(int ac, char **av)
+{
+	int	i;
+
+	i = 1;
+	ft_advanced_sort_string_tab(&av[1], ft_strcmp);
+	while (i < ac)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return (0);
+}
