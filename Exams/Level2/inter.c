865b0f825ebe7967002b5a86da8c5f4de7ec5b12
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-24 15:37:14 +0200

    Rendu de l'exercice inter

--- Exams/Level2/inter.c
+++ Exams/Level2/inter.c
@@ -5,42 +5,44 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int		check_char(char *str, char c, int index)
+{
+	int	i;
+	
+	i = 0;
+	while (i < index)
+	{
+		if (str[i] == c)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	inter(char *s1, char *s2)
 {
-<<<<<<< HEAD
-	
-=======
-	char	tab[256] = {0};
-	int		i;
-	int		j;
+	int	i;
+	int	len;
 
 	i = 0;
-	j = 0;
+	len = 0;
+	while (s2[len] != '\0')
+		len++;
 	while (s1[i] != '\0')
 	{
-		if (tab[s1[i]] == 0)
+		if (check_char(s1, s1[i], i) == 1)
 		{
-			while (s2[j] != '\0' && s1[i] != s2[j])
-			{
-				j++;
-			}
-			if (s1[i] == s2[j])
+			if (check_char(s2, s1[i], len) == 0)
 			{
 				ft_putchar(s1[i]);
-				j++;
 			}
-			tab[s1[i]] = 1;
 		}
 		i++;
 	}
->>>>>>> 286763e82c0c4905f8b9a5b915f55fef37824406
 }
 
 int	main(int ac, char **av)
 {
-	int	i;
-
-	i = 0;
 	if (ac == 3)
 	{
 		inter(av[1], av[2]);
