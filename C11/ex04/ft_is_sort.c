488e8c8f4e88c5d3f784577d55ab09b5ebd84c0c
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-24 11:30:42 +0200

    Rendu de l'exercice ft_is_sort

--- C11/ex04/ft_is_sort.c
+++ C11/ex04/ft_is_sort.c
@@ -6,7 +6,7 @@
 /*   By: mrouabeh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/22 08:41:43 by mrouabeh          #+#    #+#             */
-/*   Updated: 2019/07/24 09:31:05 by mrouabeh         ###   ########.fr       */
+/*   Updated: 2019/07/24 11:30:25 by mrouabeh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 
 	i = 0;
 	if (length == 0)
-		return (0);
+		return (1);
 	else if (length == 1)
 		return (1);
 	while (i < (length - 1) && (*f)(tab[i], tab[i + 1]) == 0)
