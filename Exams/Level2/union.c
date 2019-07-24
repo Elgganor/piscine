e4988817d1515adba27f0b01a34126d90646f1ad
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-24 15:14:59 +0200

    Rendu de l'exercice union

--- Exams/Level2/union.c
+++ Exams/Level2/union.c
@@ -5,34 +5,47 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int	main(int ac, char **av)
+int		char_isvalid(char c, char *str, int index)
 {
-	char	tab[256] = {0};
 	int	i;
 
 	i = 0;
-	if (ac == 3)
+	while (i < index)
 	{
-		while (av[1][i] != '\0')
-		{
-			if (tab[av[1][i]] == 0)
-			{
-				tab[av[1][i]] = 1;
-				ft_putchar(av[1][i]);
-			}
-			i++;
-		}
-		i = 0;
-		while (av[2][i] != '\0')
-		{
-			if (tab[av[2][i]] == 0)
-			{
-				tab[av[2][i]] = 1;
-				ft_putchar(av[2][i]);
-			}
-			i++;
-		}
+		if	(str[i] == c)
+			return (0);
+		i++;
 	}
-	write(1, "\n", 1);
+	return (1);
+}
+
+void	ft_union(char *s1, char *s2)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		if (char_isvalid(s1[i], s1, i) == 1)
+			ft_putchar(s1[i]);
+		i++;
+	}
+	len  = i;
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		if (char_isvalid(s2[i], s2, i) == 1)
+			if (char_isvalid(s2[i], s1, len) == 1)
+				ft_putchar(s2[i]);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+		ft_union(av[1], av[2]);
+	write(1 , "\n", 1);
 	return (0);
 }
