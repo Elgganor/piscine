3ff72d28e320ce675c93c065bf297356bc8bf4eb
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-24 10:10:43 +0200

    Rendu de l'exercice add_prime_sum

--- Exams/Level3/add_prime_sum.c
+++ Exams/Level3/add_prime_sum.c
@@ -43,13 +43,35 @@ int	ft_is_prime(int nb)
 	int	i;
 
 	if (nb == 0 || nb == 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	i = 2;
+	while (i <= nb / 2 && i < 46340)
+	{	
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	main(int ac, char **av)
 {
+	int	res;
+	int	i;
+
+	res = 0;
+	i = 0;
 	if (ac == 2)
 	{
-		ft
+		while (i <= ft_atoi(av[1]))
+		{
+			if (ft_is_prime(i) == 1)
+				res += i;
+			i++;
+		}
+		ft_putnbr(res);
 	}
 	ft_putchar('\n');
 	return (0);
