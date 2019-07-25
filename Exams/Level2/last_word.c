6f3b7346d38758f3f5d18aecd2e1550a7af1ff41
Mohamed-Younes <mohamed.rouabehi.pro@gmail..com>
2019-07-25 09:48:32 +0200

    Rendu de l'exercice last_word

--- Exams/Level2/last_word.c
+++ Exams/Level2/last_word.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+int		ft_is_printable(char c)
+{
+	if (33 <= c && c <= 126)
+		return (1);
+	return (0);
+}
+
 void	last_word(char *str)
 {
 	int	i;
@@ -11,17 +18,16 @@ void	last_word(char *str)
 	{
 		if (str[i] == ' ' || str[i] == '\t')
 		{
-			if (str[i + 1] != ' ' && str[i] != '\t')
+			if (ft_is_printable(str[i + 1]) == 1)
 			{
 				j = i + 1;
 			}
 		}
 		i++;
-		while (str[j] != ' ' && str[j] != '\t')
-		{
-			write(1, &str[j], 1);
-			j++;
-		}
+	}
+	while (str[j] != '\0' && ft_is_printable(str[j]) == 1)
+	{
+		write(1, &str[j++], 1);
 	}
 }
 
